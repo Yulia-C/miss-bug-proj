f6cc5492a3ca365af2b751cf08d93e5e7b2360a7
Yulia C <yulka1355@gmail.com>
2025-08-03 15:17:33 +0300

    Add a cookie for usage limit

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 import express from 'express'
+import cookieParser from 'cookie-parser'
 import { bugService } from './services/bug.service.js'
 import { loggerService } from './services/logger.service.js'
 
 const app = express()
 app.use(express.static('public'))
+app.use(cookieParser())
 
-app.get('/', (req, res) =>
-    res.send('Welcome to MissBug server...'))
+app.get('/', (req, res) => {
 
+    res.send('Welcome to MissBug server...')
+})
 
 // list
 app.get('/api/bug', (req, res) => {
@@ -37,7 +40,20 @@ app.get('/api/bug/save', (req, res) => {
 // read
 app.get('/api/bug/:id', (req, res) => {
     const bugId = req.params.id
+    var visitedBugs = req.cookies.visitedBugs || []
 
+    if (!visitedBugs.includes(bugId)) {
+        visitedBugs.push(bugId)
+    }
+
+    if (visitedBugs.length > 3) {
+        return res.status(401).send('Wait for a bit')
+    }
+
+    console.log('User visited at the following bugs:', visitedBugs)
+
+    res.cookie('visitedBugs', visitedBugs, { maxAge: 1000 * 7 })
+  
     bugService.getById(bugId)
         .then(bug => res.send(bug))
         .catch(err => {
