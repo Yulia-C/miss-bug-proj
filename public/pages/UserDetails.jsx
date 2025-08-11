f0367151964ea33c62d9ffcbfbec0ac205b2570e
Yulia C <yulka1355@gmail.com>
2025-08-11 23:18:41 +0300

    Add  users' bugs list to details with onRemove and onEdit

--- public/pages/UserDetails.jsx
+++ public/pages/UserDetails.jsx
@@ -1,16 +1,22 @@
 const { useState, useEffect } = React
 const { useParams, useNavigate, Link } = ReactRouterDOM
 
+import { showSuccessMsg, showErrorMsg } from "../services/event-bus.service.js"
+import { BugList } from "../cmps/BugList.jsx"
 import { userService } from "../services/user.service.js"
+import { bugService } from "../services/bug.service.js"
 
-export function UserDetails() {
+export function UserDetails({ onRemoveBug, onEditBug }) {
 
     const [user, setUser] = useState(null)
+    const [userBugs, setUserBugs] = useState([])
+
     const params = useParams()
     const navigate = useNavigate()
 
     useEffect(() => {
         loadUser()
+        loadUserBugs()
     }, [params.userId])
 
     function loadUser() {
@@ -22,6 +28,49 @@ export function UserDetails() {
             })
     }
 
+    function loadUserBugs() {
+        bugService.query({ userId: params.userId })
+            .then(bugs => {
+                setUserBugs(bugs)
+                console.log('bugs:', bugs)
+            })
+            .catch(err => console.log('err:', err))
+    }
+
+    function onRemoveBug(bugId) {
+        bugService
+            .remove(bugId)
+            .then(() => {
+                console.log('Deleted Succesfully!')
+                setUserBugs(prevBugs => prevBugs.filter(bug => bug._id !== bugId))
+                showSuccessMsg('Bug removed')
+            })
+            .catch(err => {
+                console.log('from remove bug', err)
+                showErrorMsg('Cannot remove bug')
+            })
+    }
+
+    function onEditBug(bug) {
+        const severity = +prompt('New severity?')
+        if (!severity) return alert('Please enter a severity')
+        const bugToSave = { ...bug, severity }
+        bugService.save(bugToSave)
+            .then(savedBug => {
+                console.log('Updated Bug:', savedBug)
+                setUserBugs(prevBugs =>
+                    prevBugs.map(currBug =>
+                        currBug._id === savedBug._id ? savedBug : currBug
+                    )
+                )
+                showSuccessMsg('Bug updated')
+            })
+            .catch(err => {
+                console.log('from edit bug', err)
+                showErrorMsg('Cannot update bug')
+            })
+    }
+
     if (!user) return <div>Loading...</div>
 
     return <section className="user-details">
@@ -30,6 +79,13 @@ export function UserDetails() {
             <h2>Username: {user.username}</h2>
             <h2>User Id: {user._id}</h2>
         </pre>
+        <section>
+            <h2>{user.fullname}'s bugs:</h2>
+            <BugList
+                bugs={userBugs}
+                onRemoveBug={onRemoveBug}
+                onEditBug={onEditBug} />
+        </section>
         <Link to="/bug">Back</Link>
     </section>
 }
